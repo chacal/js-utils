69807dec99623bd4ac0ed6556d0461474cf32801
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2019-06-25 22:52:41 +0300

    Update to Bacon.js 3.0.3

--- src/Mqtt.ts
+++ src/Mqtt.ts
@@ -1,9 +1,5 @@
 import mqtt = require('mqtt')
-import Bacon = require('baconjs')
-
-declare module 'baconjs' {
-  function fromEvent<E, A>(target: EventTarget|NodeJS.EventEmitter|JQuery, eventName: string, eventTransformer: (t: string, m: Buffer) => A): Bacon.EventStream<E, A>;
-}
+import { EventStream, fromEvent } from 'baconjs'
 
 export namespace Mqtt {
 
@@ -12,13 +8,13 @@ export namespace Mqtt {
   export function startMqttClient<A>(brokerUrl: string, username?: string, password?: string): mqtt.Client {
     let disconnectCount = 0
 
-    const client = mqtt.connect(brokerUrl, {username, password})
+    const client = mqtt.connect(brokerUrl, { username, password })
     client.on('connect', () => console.log(`Connected to MQTT server ${brokerUrl}`))
     client.on('offline', () => {
       disconnectCount++
       console.log(`Disconnected from MQTT server. Disconnect count: ${disconnectCount}/${MAX_DISCONNECT_COUNT}`)
-      if(disconnectCount === MAX_DISCONNECT_COUNT) {
-        console.log("Exiting due to too many MQTT disconnects..")
+      if (disconnectCount === MAX_DISCONNECT_COUNT) {
+        console.log('Exiting due to too many MQTT disconnects..')
         process.exit(1)
       }
     })
@@ -27,7 +23,7 @@ export namespace Mqtt {
     return client
   }
 
-  export function messageStreamFrom<E>(mqttClient: mqtt.Client): Bacon.EventStream<E, Buffer> {
-    return Bacon.fromEvent(mqttClient, 'message', (topic, msg) => msg)
+  export function messageStreamFrom<E>(mqttClient: mqtt.Client): EventStream<Buffer> {
+    return fromEvent(mqttClient, 'message', (topic, msg) => msg)
   }
 }
--- src/NetworkDisplay.ts
+++ src/NetworkDisplay.ts
@@ -1,5 +1,5 @@
 import { parse } from 'url'
-import Bacon = require('baconjs')
+import { EventStream, once, interval, fromPromise } from 'baconjs'
 import { SensorEvents } from './ISensorEvent'
 import { Coap } from './Coap'
 import IThreadParentInfo = SensorEvents.IThreadParentInfo
@@ -8,11 +8,11 @@ export namespace NetworkDisplay {
 
   type DisplayTag = 'd'
   export type DisplayStatus = { instance: string, tag: DisplayTag, vcc: number, ts: string, parent: IThreadParentInfo }
-  export type StatusStream = Bacon.EventStream<any, DisplayStatus>
+  export type StatusStream = EventStream<DisplayStatus>
 
   export function statusesWithInterval(displayAddress: string, intervalMs: number): StatusStream {
-    return Bacon.once('').concat(Bacon.interval(intervalMs, ''))
-      .flatMapLatest(() => Bacon.fromPromise(Coap.getJson(parse(`coap://[${displayAddress}]/api/status`))))
+    return once('').concat(interval(intervalMs, ''))
+      .flatMapLatest(() => fromPromise(Coap.getJson(parse(`coap://[${displayAddress}]/api/status`))))
       .map(res => JSON.parse(res.payload.toString()))
       .map(ds => ({
         instance: ds.instance,
